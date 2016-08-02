a772b74492f92189d4ad16a14e1a225903af84a3
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2016-08-02 10:32:08 -0700

    post code review repairs to random image generator

--- JS/app.js
+++ JS/app.js
@@ -4,6 +4,8 @@
 
 var allImages = [];
 var randomNumberArray = [];
+var lastNumberArray = [];
+var surveyLength = 0;
 var imageDisplay = document.getElementById('imageDisplay');
 
 //OBJECT CONSTRUCTOR
@@ -16,26 +18,26 @@ function ImageFinder(name, filePath, displayCount, voteCount){
 }
 
 // OBJECT INSTANCES CREATING OBJECTS FOR EACH IMAGE FILE
-var bag = new ImageFinder('Bag', 'img/bag.jpg', 0, 0);
-var banana = new ImageFinder('Banana', 'img/banana.jpg', 0, 0);
-var bathroom = new ImageFinder('Bathroom', 'img/bathroom.jpg', 0, 0);
-var boots = new ImageFinder('Boots', 'img/boots.jpg', 0, 0);
-var breakfast = new ImageFinder('Breakfast', 'img/breakfast.jpg', 0, 0);
-var bubblegum = new ImageFinder('Bubblegum', 'img/bubblegum.jpg', 0, 0);
-var chair = new ImageFinder('Chair', 'img/chair.jpg', 0, 0);
-var cthulhu = new ImageFinder('Cthulhu', 'img/cthulhu.jpg', 0, 0);
-var dogDuck = new ImageFinder('Dog Duck', 'img/dog-duck.jpg', 0, 0);
-var dragon = new ImageFinder('Dragon', 'img/dragon.jpg', 0, 0);
-var pen = new ImageFinder('Pen', 'img/pen.jpg', 0, 0);
-var petSweep = new ImageFinder('Pet Sweep', 'img/pet-sweep.jpg', 0, 0);
-var scissors = new ImageFinder('Scissors', 'img/scissors.jpg', 0, 0);
-var sweep = new ImageFinder('Sweep', 'img/sweep.jpg', 0, 0);
-var shark = new ImageFinder('Shark', 'img/shark.jpg', 0, 0);
-var tauntaun = new ImageFinder('Tauntaun', 'img/tauntaun.jpg', 0, 0);
-var unicorn = new ImageFinder('Unicorn', 'img/unicorn.jpg', 0, 0);
-var usb = new ImageFinder('USB', 'img/usb.gif', 0, 0);
-var waterCan = new ImageFinder('Water Can', 'img/water-can.jpg', 0, 0);
-var wineGlass = new ImageFinder('Wine Glass', 'img/wine-glass.jpg', 0, 0);
+var bag = new ImageFinder('Bag', 'img/bag.jpg');
+var banana = new ImageFinder('Banana', 'img/banana.jpg');
+var bathroom = new ImageFinder('Bathroom', 'img/bathroom.jpg');
+var boots = new ImageFinder('Boots', 'img/boots.jpg');
+var breakfast = new ImageFinder('Breakfast', 'img/breakfast.jpg');
+var bubblegum = new ImageFinder('Bubblegum', 'img/bubblegum.jpg');
+var chair = new ImageFinder('Chair', 'img/chair.jpg');
+var cthulhu = new ImageFinder('Cthulhu', 'img/cthulhu.jpg');
+var dogDuck = new ImageFinder('Dog Duck', 'img/dog-duck.jpg');
+var dragon = new ImageFinder('Dragon', 'img/dragon.jpg');
+var pen = new ImageFinder('Pen', 'img/pen.jpg');
+var petSweep = new ImageFinder('Pet Sweep', 'img/pet-sweep.jpg');
+var scissors = new ImageFinder('Scissors', 'img/scissors.jpg');
+var sweep = new ImageFinder('Sweep', 'img/sweep.jpg');
+var shark = new ImageFinder('Shark', 'img/shark.jpg');
+var tauntaun = new ImageFinder('Tauntaun', 'img/tauntaun.jpg');
+var unicorn = new ImageFinder('Unicorn', 'img/unicorn.jpg');
+var usb = new ImageFinder('USB', 'img/usb.gif');
+var waterCan = new ImageFinder('Water Can', 'img/water-can.jpg');
+var wineGlass = new ImageFinder('Wine Glass', 'img/wine-glass.jpg');
 
 
 //RANDOM NUMBER GENERATOR
@@ -49,7 +51,9 @@ function randomNumberGenerator (){
   console.log(randomNumber1);
   console.log(randomNumber2);
   //MAKING SURE NO TWO NUMBERS ARE IDENTICAL. IF IDENTICAL, LOOP AGAIN. OTHERWISE, PUSH TO ARRAY
-  if (randomNumber !== randomNumber1 && randomNumber !== randomNumber2 && randomNumber1 !== randomNumber2){
+  if (randomNumber !== randomNumber1 && randomNumber !== randomNumber2
+    && randomNumber1 !== randomNumber2){
+    randomNumberArray = [];
     randomNumberArray.push(randomNumber, randomNumber1, randomNumber2);
     // console.log('Three unique numbers');
   } else {
@@ -58,10 +62,9 @@ function randomNumberGenerator (){
   };
 };
 
-
-
 //FUNCTION TO FILL EMPTY <img> HTML ELEMENTS WITH RANDOMLY GENERATED IMAGES FROM THE IMAGE ARRAY
 function displayImages(){
+  randomNumberGenerator();
   var leftImage = document.getElementById('left');
   leftImage.src = allImages[randomNumberArray[0]].filePath;
   // console.log('this is', allImages[randomNumberArray[0]].filePath);
